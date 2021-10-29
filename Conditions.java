6a96c76669e1a1c613fc8f8cd56fac6d9a48be7b
Matt Callaghan <mcallaghan@naperville203.org>
2021-10-29 13:31:39 -0500

    started notes on conditions

--- Conditions.java
+++ Conditions.java
@@ -3,10 +3,70 @@ import java.util.Scanner;
 /**
  * Examples and notes on conditions (Chapter 5)
  *
- * @author gcschmit
- * @version 16nov2020
+ * @author mrcallaghan
+ * @version 29oct2021
  */
 public class Conditions
 {
+    public static void ifExample()
+    {
+        // model a coin flip; 1: heads, 0: tails
+        int coinFlip = (int)(Math.random() * 2);
+        
+        /*
+         * if statment
+         *  Statements are executed if the conditional expression is true.
+         *  
+         * Conditional expressions evaluate to either true or false.
+         * The conditional expression must go inside of parentheses.
+         * Statements are grouped by blocks (i.e., {}), not indentation like in python.
+         * There is no colon after the conditional expresion, unlike python.
+         * 
+         */
+        
+        if (coinFlip == 1)
+        {
+            System.out.println("Coin is heads!");
+        }
+        
+        /*
+         * if-else statement
+         *  The else block is executed if the condition evaluates to false.
+         */
+        if (coinFlip == 1)
+        {
+            System.out.println("Coin is heads!");
+        }
+        else
+        {
+            System.out.println("Coin is tails!");
+            System.out.println("Better luck next time...");
+        }
     
+        // model the roll of a four-sided die [1-4]
+        int dieRoll = (int)(Math.random() * 4) + 1;
+        
+        /*
+         * if-else if-else statement
+         *  In Java, "else if" is used instead of "elif" like Python.
+         */
+        if (dieRoll == 1)
+        {
+            System.out.println("rolled a one");
+        }
+        else if (dieRoll == 2)
+        {
+            System.out.println("rolled a two");
+        }
+        else if (dieRoll == 3)  // can have as many as needed
+        {
+            System.out.println("rolled a three");
+        }
+        else
+        {
+            System.out.println("rolled a four");
+        }
+        
+        
+    }
 }
