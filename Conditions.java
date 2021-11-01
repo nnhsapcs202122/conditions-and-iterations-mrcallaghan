e72e66851308b3802841aaa93a88a848fd6129bc
Matt Callaghan <mcallaghan@naperville203.org>
2021-11-01 13:16:38 -0500

    almost finished conditions notes

--- Conditions.java
+++ Conditions.java
@@ -67,6 +67,119 @@ public class Conditions
             System.out.println("rolled a four");
         }
         
+        /*
+         *  { } are not required for a single statement.
+         *          However, they are always a good idea.
+         *          Leaving them out can lead to bug, like this one:
+         */
+        if (coinFlip == 0)
+            System.out.println("Coin is tails!");
+            System.out.println("Better luck next time. ");
         
     }
+    
+    public static void stringExample()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter two words: ");
+        String str1 = s.next();
+        String str2 = s.next();
+        
+        /*
+         * The equality operator (==) returns true if the two variables (e.g., str1 and str2) contain
+         *      the same value. For variables of a class type, including Strings, this means that
+         *      they contain the same reference. That is, they refer to the same object in memory,
+         *      not that two Strings have the same sequence of characters.
+         */
+        if (str1 == str2)
+        {
+            System.out.println("string references are equal");
+        }
+        else
+        {
+            System.out.println("string references are not equal");
+        }
+        
+        
+        /*
+         * The equals method returns true if the two objects referenced by the variables are "equal".
+         *      What equal means is defined by the class. For Strings, it means that the two objects
+         *      have the same sequence of characters.
+         */
+        if (str1.equals(str2))
+        {
+            System.out.println("strings are equal");
+        }
+        else
+        {
+            System.out.println("strings are not equal");
+        }
+        
+        /*
+         * We will determine which string comes first lexicographically using the compareTo method
+         *      of the String class.
+         *      
+         *      compareTo returns an int:
+         *      0:      if the strings are equal (same sequence of characters)
+         *      < 0:    if str1 < str2 lexicographically
+         *      > 0:    if str1 > str2 lexicographically
+         */
+        
+        int result = str1.compareTo(str2);
+        
+        String firstStr = null;
+        
+        if(result < 0)
+        {
+            firstStr = str1;
+        }
+        else if (result > 0)
+        {
+            firstStr = str2;
+        }
+        
+        if(firstStr != null)
+        {
+            System.out.println("The first string is: " + firstStr);
+            System.out.println("Its length is: " + firstStr.length());
+        }
+        else
+        {
+            System.out.println("strings are equal");
+            //System.out.println("Its length is: " + firstStr.length());
+        }
+        
+        /*
+         * This is an example of a "short circuit".
+         * 
+         * If the left operand is false, the right operand will not be evaluated
+         *      because the AND operation is already false.
+         */
+        if(firstStr != null && firstStr.length() > 3)
+        {
+            System.out.println("The first string has more than 3 characters.");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
